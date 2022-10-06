b78f51255882d49f28c9b9730a317cda9c447e6f
hxxtae <fkdlxmfkdl@gmail.com>
2022-10-06 13:47:30 +0900

    :books: [Docs]: Add introduce text
    - add introduce text with console.log

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,18 @@ import { menuClick, menuIconClick, overlayClick } from './menuEvent.js';
 menuClick();
 menuIconClick();
 overlayClick();
+
+const name = "'Hxxtae'";
+console.log(' %c /* Welcome to the %c%s%c website */ ', `
+  color: #e7e7e7; 
+  background-color: black;
+  line-height: 44px`, `
+  color: hotpink;
+  background-color: black;
+  line-height: 44px;
+  font-weight: bold;
+  font-size: 14px`,
+  name,`
+  color: #e7e7e7;
+  background-color: black;
+  line-height: 44px`);
